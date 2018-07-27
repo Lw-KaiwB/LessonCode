2fd4e9e156a208491f1b6ab255a2e95302f792c4
Lw-KaiwB <2596536983@qq.com>
2018-07-27 11:24:11 +0800

    Initial commit

---
+++ app/src/main/java/com/kb/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.kb.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kb.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
